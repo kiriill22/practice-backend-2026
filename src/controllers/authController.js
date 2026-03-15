f011b68ce106d5f9302ec2fe64b8b4e07c63306e
kirillkopylov50-afk <kirillkopylov50@gmail.com>
2026-03-15 18:47:02 +0700

    stage4

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,34 @@
-exports.getAll = (req, res) => {
-    res.json({ message: 'Список компьютеров из базы' });
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = 'super_secret_key_2026';
+
+exports.register = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await prisma.user.create({
+            data: { username, passwordHash: hashedPassword, role: 'player' }
+        });
+        res.status(201).json({ message: "Пользователь успешно зарегистрирован", userId: user.id });
+    } catch (error) {
+        res.status(400).json({ error: "Ошибка регистрации" });
+    }
 };
 
-exports.create = (req, res) => {
-    res.json({ message: 'Компьютер успешно добавлен в базу' });
+exports.login = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await prisma.user.findUnique({ where: { username } });
+        if (!user) return res.status(401).json({ error: "Пользователь не найден" });
+
+        const isMatch = await bcrypt.compare(password, user.passwordHash);
+        if (!isMatch) return res.status(401).json({ error: "Неверный пароль" });
+
+        const token = jwt.sign({ id: user.id, role: user.role }, JWT_SECRET, { expiresIn: '1h' });
+        res.json({ message: "Успешный вход", token });
+    } catch (error) {
+        res.status(500).json({ error: "Ошибка сервера" });
+    }
 };
--- src/server.js
+++ src/server.js
@@ -3,6 +3,11 @@ const app = express();
 
 app.use(express.json());
 
+const authRoutes = require('./routes/authRoutes');
+app.use('/auth', authRoutes);
+
+app.use(express.json());
+
 app.use('/api/computers', require('./routes/computerRoutes'));
 
 app.listen(3000, () => console.log('Сервер запущен на порту 3000'));
